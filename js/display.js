c3bf30a310fb2b7cf799d6f20b7667f34c6c5d6a
Derek Rhodes <physci@gmail.com>
2015-11-23 11:58:17 -0500

    getting ready to add other courses

--- js/display.js
+++ js/display.js
@@ -2,134 +2,171 @@
 //
 var nil = null;
 
-function hasDirectiveWE(terms) {
-    return $.inArray("#we", terms) != -1;
-}
 
-function hasDirectiveLEC(terms) {
-    return $.inArray("#lec", terms) != -1;
-}
-
-function removeDirectives(terms) {
-    return terms
-        .filter(function(x) { return x != "#lec"; })
-        .filter(function(x) { return x != "#we"; }); 
-}
-
-// slow linear search is good enough for now.    
-// Terms -> [items that contain terms]
-function searchForTerms(lecId, searchTerms) {
-    if (searchTerms == nil) {
-        return [];
+function SearchModule() {
+    // everything in this function is private except the last
+    // function, which is returned as an interface to the search
+    // functionality
+    
+    function hasDirectiveWE(terms) {
+        return $.inArray("#we", terms) != -1;
     }
-    
-    var lec = JSON_transcripts[lecId];
-    var items = lec.items;
-    searchTerms = searchTerms.toLowerCase();
-    terms = searchTerms.split(" ");
 
-    // which directives are set?
-    var workedExamplesOnly = hasDirectiveWE(terms);
-    var lecturesOnly = hasDirectiveLEC(terms);
-    terms = removeDirectives(terms);
-
-    var results = [];
-
-    // check to see if item.text contains all terms
-    items.forEach(function(item) {
-        txt = item.text.toLowerCase();
-        // iterate over terms
-
-        var allTermsMatch = true;
-        terms.forEach(function(term) {
-            if (txt.indexOf(term) == -1) {
-                // couldn't find this term in the item text
-                allTermsMatch = false;
-                return;
-            } 
-        });
-
-        if (allTermsMatch) {
-            if (workedExamplesOnly && item.is_worked_example) {
-                results.push(item);
-            }
-            if (lecturesOnly && item.is_lecture) {
-                results.push(item);
-            }
-            if (!lecturesOnly && !workedExamplesOnly) {
-                results.push(item);
-            }
-        }            
-    });
-    
-    return results;
-}
-
-function generateDiv(url, item) { // -> string
-    var resultStr = "";
-    
-    resultStr += "<hr>";
-    resultStr += "<p>" + item.text + "</p>";
-
-    if (item.is_worked_example) {
-        resultStr += "<span>worked example <a href='" + url + "'>lecture</a><span>";
-    } else { 
-        resultStr += "<span><a href='" + url + "'>lecture</a><span>";
+    function hasDirectiveLEC(terms) {
+        return $.inArray("#lec", terms) != -1;
     }
-    resultStr += " from time: ";
-    resultStr += item.start; 
-    resultStr += " to ";
-    resultStr += item.end;
-    return resultStr;
-}
 
-function insertResults(resultStrings) { // [string] -> DOM ()
-    // concat the results
-    var catted = resultStrings.join("");
-    var results = $("#results");
-    results.append(catted);
-}
+    function removeDirectives(terms) {
+        return terms
+            .filter(function(x) { return x != "#lec"; })
+            .filter(function(x) { return x != "#we"; }); 
+    }
 
-function clearResultsDiv() {
-    $("#results").empty();
-}
-
-function refreshResults(terms) {
-    clearResultsDiv();
-    var lectures = Object.keys(JSON_transcripts);
-    var numFound = 0;
-    const limit = 25;
-    var accum = [];
-    
-    lectures.forEach(function(lecId){
-        if (accum.length >= limit) {                    
-            return;
+    // slow linear search is good enough for now.    
+    // Terms -> [items that contain terms]
+    function searchForTerms(lecId, searchTerms) {
+        if (searchTerms == nil) {
+            return [];
         }
         
-        var lecture = JSON_transcripts[lecId];
-        var results = searchForTerms(lecId, terms);
-        if (results.length!=0) {
-            numFound += results.length;
-            results.forEach(function(item){
-                accum.push(generateDiv(lecture.url, item));
-            });            
-        }
-    });
-    
-    insertResults(accum);
+        var lec = JSON_transcripts[lecId];
+        var items = lec.items;
+        searchTerms = searchTerms.toLowerCase();
+        terms = searchTerms.split(" ");
 
-    if (accum.length > limit) {
-        return "more than " + numFound;
+        // which directives are set?
+        var workedExamplesOnly = hasDirectiveWE(terms);
+        var lecturesOnly = hasDirectiveLEC(terms);
+        terms = removeDirectives(terms);
+
+        var results = [];
+
+        // check to see if item.text contains all terms
+        items.forEach(function(item) {
+            txt = item.text.toLowerCase();
+            // iterate over terms
+
+            var allTermsMatch = true;
+            terms.forEach(function(term) {
+                if (txt.indexOf(term) == -1) {
+                    // couldn't find this term in the item text
+                    allTermsMatch = false;
+                    return;
+                } 
+            });
+
+            if (allTermsMatch) {
+                if (workedExamplesOnly && item.is_worked_example) {
+                    results.push(item);
+                }
+                if (lecturesOnly && item.is_lecture) {
+                    results.push(item);
+                }
+                if (!lecturesOnly && !workedExamplesOnly) {
+                    results.push(item);
+                }
+            }            
+        });
+        
+        return results;
     }
-    return "exactly " + numFound;
+
+    function generateDiv(url, item) { // -> string
+        var resultStr = "";
+        
+        resultStr += "<hr>";
+        resultStr += "<p>" + item.text + "</p>";
+
+        if (item.is_worked_example) {
+            resultStr += "<span>worked example <a href='" + url + "'>lecture</a><span>";
+        } else { 
+            resultStr += "<span><a href='" + url + "'>lecture</a><span>";
+        }
+        resultStr += " from time: ";
+        resultStr += item.start; 
+        resultStr += " to ";
+        resultStr += item.end;
+        return resultStr;
+    }
+
+    // take a list of strings, concat them and insert them into the dom.
+    function insertResults(resultStrings) { // [string] -> DOM ()
+        // concat the results
+        var catted = resultStrings.join("");
+        var results = $("#results");
+        results.append(catted);
+    }
+
+    // clear out the results div
+    function clearResultsDiv() {
+        $("#results").empty();
+    }
+
+    // This is the only function that needs to be visible.
+    function refreshResults(terms, course) {
+        // As of now, this course variable is ignored.  Soon it will
+        // be used to select the appropriate JSON_transcripts.
+        
+        clearResultsDiv();
+        var lectures = Object.keys(JSON_transcripts);
+        var numFound = 0;
+        const limit = 25;
+        var accum = [];
+        
+        lectures.forEach(function(lecId){
+            if (accum.length >= limit) {                    
+                return;
+            }
+            
+            var lecture = JSON_transcripts[lecId];
+            var results = searchForTerms(lecId, terms);
+            if (results.length!=0) {
+                numFound += results.length;
+                results.forEach(function(item){
+                    accum.push(generateDiv(lecture.url, item));
+                });            
+            }
+        });
+        
+        insertResults(accum);
+
+        if (accum.length > limit) {
+            return "more than " + numFound;
+        }
+        return "exactly " + numFound;
+    }
+
+    return refreshResults;
 }
+
+
+// https://stackoverflow.com/questions/8486099
+// thanks @ Jan Turoň
+function getJsonFromUrl() {
+    var query = location.search.substr(1);
+    var result = {};
+    query.split("&").forEach(function(part) {
+        var item = part.split("=");
+        result[item[0]] = decodeURIComponent(item[1]);
+    });
+    return result;
+}
+
 
 var app = angular.module('app', []);
 app.controller('myCtrl', function($scope) {
-    $scope.search = function() {
-        return refreshResults($scope.terms);
-    };
+    var course = "6.004.2x";
+
+    // Eventually uncomment this code and use the params to 
+    // var params = getJsonFromUrl();
+    // if (params.course != "6.004.2x") {
+    //     // course isn't defined.
+    //     alert("Search isn't implemented for course: " + params.course);
+    // }
     
+    var refreshResults = SearchModule();
+    
+    $scope.search = function() {
+        return refreshResults($scope.terms, course);
+    };    
 });
-
-
